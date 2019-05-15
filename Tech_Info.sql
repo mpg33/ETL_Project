1f7b21c7020ffa7f418aa5d5c04fe69040c16ac4
Mike <47514894+mpg33@users.noreply.github.com>
2019-05-15 10:27:59 -0700

    Add files via upload

---
+++ Tech_Info.sql
@@ -0,0 +1,18 @@
+USE Tech;
+
+-- Create tables for raw data to be loaded into
+CREATE TABLE Tech_Info (
+  Company VARCHAR(30) PRIMARY KEY NOT NULL,
+  url  VARCHAR(30) NOT NULL,
+  Category VARCHAR(30) NOT NULL,
+  Sub_Category VARCHAR(30) NOT NULL,
+  industry VARCHAR(30) NOT NULL,
+  financials_arr_max INT NOT NULL,
+  financials_arr_min INT NOT NULL,
+  financials_revenue_2016 INT NOT NULL,
+  financials_revenue_2017 FLOAT NOT NULL,
+  financials_num_customers_max FLOAT NOT NULL,
+  financials_num_customers_min FLOAT NOT NULL
+);
+
+SELECT * FROM Tech_Info;
